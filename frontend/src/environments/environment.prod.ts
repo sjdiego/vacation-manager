f1c6ba060d940f11a22240b76ade9e38743c9595
Diego <diego@diego.cat>
2025-12-24 17:20:00 +0100

    Update production environment configuration with correct clientId and redirectUri

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'https://vacation-manager-api.azurewebsites.net/api',
   auth: {
-    clientId: 'bb4ff7a1-4b89-4fb4-9e1c-9357ff252613',
+    clientId: 'f78c1a6a-77c6-4601-830d-ce0e031b1a58',
     authority: 'https://login.microsoftonline.com/f1496c44-cb7c-4498-84e1-4704ec4716e9',
-    redirectUri: 'https://vacation-manager-frontend.azurestaticapps.net/'
+    redirectUri: 'https://thankful-pond-0eb655f03.4.azurestaticapps.net/'
   }
 };
