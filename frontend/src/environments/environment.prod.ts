b9297c87c5714739cd92bc1e5b8e8900656edc13
Diego <diego@diego.cat>
2025-12-27 21:51:02 +0100

    Refactor deployment workflow to replace environment variable placeholders and remove obsolete configuration step

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'API_URL_PLACEHOLDER',
   auth: {
     clientId: 'f78c1a6a-77c6-4601-830d-ce0e031b1a58',
     authority: 'https://login.microsoftonline.com/f1496c44-cb7c-4498-84e1-4704ec4716e9',
-    redirectUri: ''
+    redirectUri: 'REDIRECT_URI_PLACEHOLDER'
   }
 };
